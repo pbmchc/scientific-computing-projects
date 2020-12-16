4eaec40398418c6f715ac961e3c85feab21f3855
PBM <pbmalecki@wp.pl>
2020-12-16 13:57:18 +0100

    Renamed time calculator private functions

--- 02-time-calculator/time_calculator.py
+++ 02-time-calculator/time_calculator.py
@@ -9,21 +9,21 @@ def add_time(start, duration, start_day=None):
     start_time, start_day_period = start.split(' ')
     start_hours, start_minutes = time_utils.split_time(start_time)
     start_hours = start_hours if start_day_period == AM else start_hours + CLOCK_HOURS
-    end_hours, end_minutes = get_end_time_parts(start_hours, start_minutes, duration)
+    end_hours, end_minutes = _get_end_time_parts(start_hours, start_minutes, duration)
 
     passed_periods = time_utils.count_passed_periods(start_hours, end_hours)
     passed_days = time_utils.count_passed_days(passed_periods, start_day_period)
 
-    end_day = get_end_day(start_day, passed_days)
-    end_day_period = get_end_day_period(passed_periods, start_day_period)
-    end_relative_part_suffix = get_end_relative_part_suffix(passed_days)
+    end_day = _get_end_day(start_day, passed_days)
+    end_day_period = _get_end_day_period(passed_periods, start_day_period)
+    end_relative_part_suffix = _get_end_relative_part_suffix(passed_days)
     end_time = time_utils.format_clock_hours(end_hours) + ':' + time_utils.format_clock_minutes(end_minutes)
     separator = ', ' if end_day else ''
 
     return (end_time + ' ' + end_day_period + separator + end_day + ' ' + end_relative_part_suffix).strip()
 
 
-def get_end_time_parts(start_hours, start_minutes, duration):
+def _get_end_time_parts(start_hours, start_minutes, duration):
     duration_hours, duration_minutes = time_utils.split_time(duration)
     end_hours = start_hours + duration_hours
     end_minutes = start_minutes + duration_minutes
@@ -35,7 +35,7 @@ def get_end_time_parts(start_hours, start_minutes, duration):
     return end_hours, end_minutes
 
 
-def get_end_day(start_day, passed_days):
+def _get_end_day(start_day, passed_days):
     if start_day is None:
         return ''
 
@@ -45,14 +45,14 @@ def get_end_day(start_day, passed_days):
     return DAYS_OF_WEEK[end_day_index]
 
 
-def get_end_day_period(passed_periods, start_day_period):
+def _get_end_day_period(passed_periods, start_day_period):
     if passed_periods % 2 == 0:
         return start_day_period
 
     return AM if start_day_period == PM else PM
 
 
-def get_end_relative_part_suffix(passed_days):
+def _get_end_relative_part_suffix(passed_days):
     if passed_days == 0:
         return ''
 
