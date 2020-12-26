17bc82933110d10462850bc910e82f8b8195ed71
PBM <pbmalecki@wp.pl>
2020-12-26 22:44:22 +0100

    Implemented creating withdrawals chart

--- 03-budget-app/budget.py
+++ 03-budget-app/budget.py
@@ -1,8 +1,13 @@
 from category import Category
-from chart_utils import get_chart_data_records
+from chart_utils import get_chart_data_records, get_chart_area, get_chart_x_axis, get_chart_labels
+
+CHART_TITLE = 'Percentage spent by category'
 
 
 def create_spend_chart(categories):
     chart_data_records = get_chart_data_records(categories)
+    chart_area = get_chart_area(chart_data_records)
+    chart_x_axis = get_chart_x_axis(chart_data_records)
+    chart_labels = get_chart_labels(chart_data_records)
 
-    return None
+    return CHART_TITLE + '\n' + '\n'.join(chart_area) + '\n' + chart_x_axis + '\n' + '\n'.join(chart_labels)
--- 03-budget-app/chart_utils.py
+++ 03-budget-app/chart_utils.py
@@ -1,6 +1,11 @@
 from number_utils import count_percentage_share
 
+CHART_BAR_BUILDING_BLOCK = 'o'
 CHART_PERCENTAGE_SHARE_STEP = 10
+CHART_X_AXIS_BUILDING_BLOCK = '-'
+CHART_Y_AXIS_TICK_LABEL_LENGTH = 4
+CHART_Y_AXIS_TICK_LABEL_VALUE_LENGTH = 3
+CHART_Y_AXIS_TICK_LABEL_SEPARATOR = '|'
 
 
 def get_chart_data_records(categories):
@@ -11,8 +16,56 @@ def get_chart_data_records(categories):
     return withdrawals_percentage_shares
 
 
+def get_chart_area(chart_records):
+    y_axis_ticks = range(100, -10, -CHART_PERCENTAGE_SHARE_STEP)
+    x_axis_length = _get_chart_x_axis_length(chart_records)
+
+    return [_get_chart_area_section(tick, chart_records, x_axis_length) for tick in y_axis_ticks]
+
+
+def get_chart_x_axis(chart_records):
+    x_axis_length = _get_chart_x_axis_length(chart_records)
+
+    return (x_axis_length * CHART_X_AXIS_BUILDING_BLOCK).rjust(x_axis_length + CHART_Y_AXIS_TICK_LABEL_LENGTH)
+
+
+def get_chart_labels(chart_records):
+    max_label_length = max([len(label) for label, _ in chart_records])
+    x_axis_length = _get_chart_x_axis_length(chart_records)
+
+    return [_get_chart_labels_section(character, chart_records, x_axis_length) for character in range(max_label_length)]
+
+
 def _get_percentage_share(record, withdrawals_total):
     name, category_total = record
     category_withdrawals_percentage_share = count_percentage_share(category_total, withdrawals_total)
 
     return name, round(category_withdrawals_percentage_share)
+
+
+def _get_chart_area_section(tick, chart_records, x_axis_length):
+    chart_y_axis_tick_label = str(tick).rjust(CHART_Y_AXIS_TICK_LABEL_VALUE_LENGTH)
+    chart_bar_part = [_get_chart_bar_part(value, tick) for _, value in chart_records]
+    chart_area_section = chart_y_axis_tick_label + CHART_Y_AXIS_TICK_LABEL_SEPARATOR + ' ' + ''.join(chart_bar_part)
+
+    return chart_area_section.ljust(x_axis_length + CHART_Y_AXIS_TICK_LABEL_LENGTH)
+
+
+def _get_chart_bar_part(value, tick):
+    return CHART_BAR_BUILDING_BLOCK + '  ' if value >= tick else '   '
+
+
+def _get_chart_labels_section(character, chart_records, x_axis_length):
+    chart_label_part = [_get_chart_label_part(label, character) for label, _ in chart_records]
+
+    return ''.join(chart_label_part).rjust(x_axis_length + CHART_Y_AXIS_TICK_LABEL_LENGTH)
+
+
+def _get_chart_label_part(label, index):
+    return label[index] + '  ' if len(label) >= index + 1 else '   '
+
+
+def _get_chart_x_axis_length(chart_records):
+    chart_records_count = len(chart_records)
+
+    return chart_records_count + chart_records_count * 2 + 1
