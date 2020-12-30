e221110a10e037fa815228ba98a135d1cf3d105e
PBM <pbmalecki@wp.pl>
2020-12-30 16:16:24 +0100

    Implemented method for drawing balls from hat

--- 05-probability-calculator/prob_calculator.py
+++ 05-probability-calculator/prob_calculator.py
@@ -7,6 +7,16 @@ class Hat:
     def __init__(self, **balls):
         self.contents = convert_to_arguments_list(balls)
 
+    def draw(self, number_of_balls):
+        max_number_of_balls = len(self.contents)
+        sample_length = number_of_balls if number_of_balls <= max_number_of_balls else max_number_of_balls
+        draw_result = random.sample(self.contents, sample_length)
+
+        for ball in draw_result:
+            self.contents.remove(ball)
+
+        return draw_result
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     return None
